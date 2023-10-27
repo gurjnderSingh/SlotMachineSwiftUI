616ec77d6fedc0fe7d9f91afd3e3a7d42debdaa0
gurjnderSingh <gurjindersingh663@gmail.com>
2023-10-27 17:35:49 +0530

    inital commit

---
+++ SlotMachine/SlotMachine/SlotMachineApp.swift
@@ -0,0 +1,17 @@
+//
+//  SlotMachineApp.swift
+//  SlotMachine
+//
+//  Created by Gurjinder Singh on 27/10/23.
+//
+
+import SwiftUI
+
+@main
+struct SlotMachineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
